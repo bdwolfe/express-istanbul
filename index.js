ded04bd6bd0c9d47755ead5545e3a68bc1e34fdf
bdwolfe <bdwolfe@users.noreply.github.com>
2019-02-06 14:34:11 -0500

    Allow cross-origin requests

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const cov = require('istanbul-middleware');
 const express = require('express');
 const app = express();
 
+// allow posting from different origin domains
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+
 app.use('/coverage', cov.createHandler()); // mount istanbul middleware here
 
 app.listen(6969);
